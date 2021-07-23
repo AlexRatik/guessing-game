bf1a1224a01443fcf1d82100ef970f0ec03b6090
AlexRatik <aleksandr-ratkevich@mail.ru>
2021-07-23 19:26:25 +0300

    feat: add solve as BS

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.i;
+        this.min;
+        this.max;
+        this.mid;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return (this.mid = Math.round((this.min + this.max) / 2));
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
